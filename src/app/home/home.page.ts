4c3748b117dcb93bd5c043ef12ecbcc658448442
miller-ryan <35612874+miller-ryan@users.noreply.github.com>
2019-04-29 20:10:45 -0500

    Shared data between pages...

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { SharedDataService } from '../shared-data.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
+
 export class HomePage {
+  constructor(private sharedDataSVC: SharedDataService){
+    
+  }
 
 }
